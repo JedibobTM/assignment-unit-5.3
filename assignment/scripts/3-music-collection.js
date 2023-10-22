7d35e89185c7218eb96f13714e098198ece5bcf3
Christian Carlson <christiancarlson648@gmail.com>
2023-10-22 15:10:10 -0500

    Couldn't figure out what was going on with the tests and code so I submitted it for feedback

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,17 +1,48 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
+var myCollection = [];
 
+function addToCollection(collection, title, artist, yearPublished) {
+  let record = {
+    collection: collection,
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  }
+  collection.push(record);
+  function showCollection(collection){
+    for (let album of record.collection) {
+      console.log(record.title, record.artist, record.yearPublished)
+    }
+  }
+  showCollection(myCollection);
 
+  return record;
+}
 
+function findByArtist(findCollection, findArtist) {
+  matching = [];
+  if (findArtist && findCollection === myCollection.collection && myCollection.artist) {
+    matching.push(collection);
+    console.log(matching);
+  }
+  else {
+    console.log("Nothing found");
+  }
+  return matching;
+}
 
-
-
-
+console.log(addToCollection(myCollection, "Name of the song: Carmen", "Artist: Lana Del Rey", "Year published: 2012"));
+console.log(addToCollection(myCollection, "Name of the song: Carolina", "Artist: Taylor Swift", "Year published: 2022"));
+console.log(addToCollection(myCollection, "Name of the song: Into the Unknown", "Artist: The Blasting Company", "Year published: 2014"));
+console.log(addToCollection(myCollection, "Name of the song: We Don't Talk About Bruno", "Artist: Lin Manuel-Miranda", "Year published: 2021"));
+console.log(addToCollection(myCollection, "Name of the song: My Heart Will Go On", "Artist: Celine Dion", "Year published: 1999"));
+console.log(addToCollection(myCollection, "Name of the song: It's The End Of The World As We Know It", "Artist: REM", "Year published: 1985"));
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
 // as a lil' chunk of friendly code that you don't need to understand right now.
-// (It's used for automated testing.)
+// (It's used for automated testing.) 
 try {
   module.exports = {
     myCollection: typeof myCollection !== 'undefined' ? myCollection : undefined,
